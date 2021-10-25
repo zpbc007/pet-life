5fa7c62159f41f701204853d90d36ef8419b3a98
zhaopeng06 <zhaopeng06@kuaishou.com>
2021-10-25 09:29:50 +0800

    feat: add icon

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,32 @@
 export default {
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/todo/index", "pages/index/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-}
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "宠记",
+    navigationBarTextStyle: "black",
+    navigationStyle: "custom",
+  },
+  tabBar: {
+    list: [
+      {
+        pagePath: "pages/todo/index",
+        text: "首页",
+        iconPath: "./public/home.png",
+        selectedIconPath: "./public/home-selected.png",
+      },
+      {
+        pagePath: "pages/index/index",
+        text: "宠物",
+        iconPath: "./public/pet-home.png",
+        selectedIconPath: "./public/pet-home-selected.png",
+      },
+      {
+        pagePath: "pages/index/index",
+        text: "设置",
+        iconPath: "./public/setting.png",
+        selectedIconPath: "./public/setting-selected.png",
+      },
+    ],
+  },
+};
